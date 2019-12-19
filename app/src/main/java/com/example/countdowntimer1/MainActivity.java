e1b574fcb55344760df6e4965585f582c44ee624
Dinh Quang <quangddinh@gmail.com>
2019-12-19 19:21:48 +0700

    View flipper 1

--- app/src/main/java/com/example/countdowntimer1/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer1/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.countdowntimer1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     int [] mArrayImages = {
@@ -16,42 +19,43 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.img2,
             R.drawable.img3,
     };
-    ImageView mImageView1;
-    Button mButtonView1;
-    int mCount = 0;
-    boolean mIsLoading = false;
 
+    Button mButtonView1;
+    ViewFlipper mViewFlipper;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageView1 = findViewById(R.id.imageView1);
+
         mButtonView1 = findViewById(R.id.buttonView1);
+        mViewFlipper = findViewById(R.id.viewflipper);
 
-        mButtonView1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mIsLoading) {
-                    return;
-                }
-                mIsLoading = true;
-                CountDownTimer countDownTimer = new CountDownTimer((mArrayImages.length * 1000) + 1000, 1000) {
-                    @Override
-                    public void onTick(long millisUntilFinished) {
-                        mImageView1.setImageResource(mArrayImages[mCount++]);
-                    }
+        for (int i = 0; i < mArrayImages.length ; i++ ){
+            ImageView imageView = new ImageView(this);
+//                RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+//                        mViewFlipper.getLayoutParams().height,
+//                        mViewFlipper.getLayoutParams().width
+//                );
+//            imageView.setLayoutParams(layoutParams);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setImageResource(mArrayImages[i]);
+            mViewFlipper.addView(imageView);
+        }
 
-                    @Override
-                    public void onFinish() {
-                        mCount = 0;
-                        this.start();
-                    }
-                };
-                countDownTimer.start();
-            }
-        });
+
+         mButtonView1.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 if (mViewFlipper.isFlipping()){
+                     return;
+                 }
+                 mViewFlipper.setFlipInterval(1000);
+                 mViewFlipper.startFlipping();
+             }
+         });
+
 
 
 
